a7b700d2857f6ec1d4a9432af21b5b54ab9d7a54
NiccoloMachiaveli <Koroliskander@yandex.ru>
2026-02-04 20:53:15 +0400

    setup database books.db
    setup database books for work with book

--- main.py
+++ main.py
@@ -1,8 +1,54 @@
-from fastapi import FastAPI, HTTPException
+from typing import Annotated
+
+from fastapi import FastAPI, HTTPException, Depends
 import uvicorn
 from pydantic import BaseModel, Field, EmailStr
 
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
 app = FastAPI()
+
+engine = create_async_engine('sqlite+aiosqlite:///books.db')
+
+new_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_session():
+    async with new_session() as session:
+        yield session
+
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
+
+class Base(DeclarativeBase):
+    pass
+
+
+class BookModel(Base):
+    __tablename__ = "books"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    author: Mapped[str]
+
+
+@app.post("/setup_database")
+async def setup_database():
+    async with engine.begin() as connect:
+        await connect.run_sync(Base.metadata.drop_all)
+        await connect.run_sync(Base.metadata.create_all)
+    return {"success": True, "message": "База данных настроенна"}
+
+
+class BookAddSchema(BaseModel):
+    title: str
+    author: str
+
+
+class BookSchema(BookAddSchema):
+    id: int
 
 
 class UserSchema(BaseModel):
@@ -12,54 +58,41 @@ class UserSchema(BaseModel):
     interests: str | None = Field(max_length=500)
 
 
-books = [
-    {
-        "id": 1,
-        "title": "Идиот",
-        "author": "Достоевский",
-    },
-    {
-        "id": 2,
-        "title": "Вишневый сад",
-        "author": "Чехов",
-    },
-]
-
 users = []
+
+
+@app.post("/books",
+          tags=["Книги"],
+          summary="Добавление новой книги в базу данных")
+async def create_book(data: BookAddSchema, session: SessionDep):
+    new_book = BookModel(
+        title=data.title,
+        author=data.author
+    )
+    session.add(new_book)
+    await session.commit()
+    return {"success": True, "message": "Книга успешно добавлена"}
 
 
 @app.get("/books",
          tags=["Книги"],
          summary="Получить все книги")
-def read_books():
-    return books
+async def read_books(session: SessionDep):
+    query = select(BookModel)
+    result = await session.execute(query)
+    return result.scalars().all()
 
 
 @app.get("/books/{id}",
          tags=["Книги"],
-         summary="Получить конкретную книгу")
-def get_book(id: int):
+         summary="Получить конкретную книгу из базы данных")
+async def get_book(id: int, session: SessionDep):
+    books = await read_books(session)
+
     for book in books:
-        if id == book["id"]:
+        if id == book.id:
             return book
     raise HTTPException(status_code=404, detail="Книга не найдена")
-
-
-class NewBook(BaseModel):
-    title: str
-    author: str
-
-
-@app.post("/books",
-          tags=["Книги"],
-          summary="Добавление новой книги")
-def create_book(new_book: NewBook):
-    books.append({
-        "id": len(books) + 1,
-        "title": new_book.title,
-        "author": new_book.author,
-    })
-    return {"success": True, "message": "Книга успешно добавлена"}
 
 
 @app.post("/users",
